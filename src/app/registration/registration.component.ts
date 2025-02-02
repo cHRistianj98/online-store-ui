b451dd8157de7975809aad5ce84d472fd47c76fa
Kania <kania.praca@wp.pl>
2025-02-02 14:01:35 +0100

    Create registration component

---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-registration',
+  imports: [CommonModule],
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent {
+
+}
